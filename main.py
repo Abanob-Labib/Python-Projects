55bb7e16d0219b0bbe9249274ebc97e8cc0baa40
Abanob-Labib <67372159+Abanob-Labib@users.noreply.github.com>
2023-09-08 11:47:03 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import operator
+
+ops={"+":operator.add , "-":operator.sub , "*":operator.mul}
+
+def arithmetic_arranger(problems,solver=True):
+    if len(problems) > 5:
+        return "Error: Too many problems."
+    top=""
+    bottom=""
+    lines=""
+    totals=""
+    
+    for n in problems:
+        fnumber=n.split()[0]
+        operator=n.split()[1]
+        snumber=n.split()[2]
+
+        if operator!="+" and operator!="-":
+            return "Error: Operator must be '+' or '-'."
+
+        if not fnumber.isdigit() or not snumber.isdigit():
+            return "Error: Numbers must only contain digits."
+
+        if len(fnumber) > 4 or len(snumber) > 4 :
+            return "Error: Numbers cannot be more than four digits."
+
+        # Get total of correct function
+        total = ops[operator](int(fnumber), int(snumber))
+        # Get distance for longest operator
+        operatorDistance = max(len(fnumber), len(snumber)) + 2
+
+        snumber = operator + snumber.rjust(operatorDistance - 1)
+        top = top + fnumber.rjust(operatorDistance) + (4 * " ")
+        bottom = bottom + snumber + (4 * " ")
+        lines = lines + len(snumber) * "_" + (4 * " ")
+        totals = totals + str(total).rjust(operatorDistance) + (4 * " ")
+
+    if solver==True:
+        print(top)
+        print(bottom)
+        print(lines)
+        print(totals)
+
+
+if __name__ == "__main__":
+    arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+
